ee01c0ee082da25dffdc1b4bbbbb2c2c1743fe98
AndiEcker <aecker2@gmail.com>
2019-10-11 21:34:29 +0100

    set initial portion version

--- ae/console.py
+++ ae/console.py
@@ -212,6 +212,9 @@ from ae.core import (
 from ae.literal import Literal
 
 
+__version__ = '0.0.1'
+
+
 INI_EXT: str = '.ini'                   #: INI file extension
 MAIN_SECTION_DEF: str = 'aeOptions'     #: default name of main config section
 
