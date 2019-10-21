6c8a02a93623a89f00c749473b9ddea1dd409593
AndiEcker <aecker2@gmail.com>
2019-10-21 15:19:50 +0100

    V 0.0.17: increased wait time in test_is_main_cfg_file_modified:
    - before_script fix for test pylint ci/cd

--- ae/console.py
+++ ae/console.py
@@ -217,7 +217,7 @@ from ae.core import (               # type: ignore  # for mypy
 from ae.literal import Literal      # type: ignore
 
 
-__version__ = '0.0.16'
+__version__ = '0.0.17'
 
 
 INI_EXT: str = '.ini'                   #: INI file extension
