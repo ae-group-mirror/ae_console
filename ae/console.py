c70997d5a910c2fccff0697728d7b06f4d3c1eff
AndiEcker <aecker2@gmail.com>
2020-01-25 18:46:49 +0000

    V 0.0.29: fixed bug (app.debug_level not set if only in config file)
    .. and added missing type annotations.

--- ae/console.py
+++ ae/console.py
@@ -235,7 +235,7 @@ from ae.core import (                   # type: ignore  # for mypy
 from ae.literal import Literal          # type: ignore
 
 
-__version__ = '0.0.28'
+__version__ = '0.0.29'
 
 
 INI_EXT: str = '.ini'                   #: INI file extension
